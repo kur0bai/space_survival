351dc6ab71f99bacddfe28020a8d1f0e124e7e39
Jesus Salcedo <jesalceg@gmail.com>
2025-05-31 08:37:06 -0500

    feature: add basic functions for the player

--- game.py
+++ game.py
@@ -16,10 +16,30 @@ class Game:
         self.canvas.pack()
 
         self.player = self.canvas.create_rectangle(
-            WIDTH//2 - 15, HEIGHT - 30, WIDTH//2 + 15, HEIGHT - 10, fill="white")
+            WIDTH//2 - 15, HEIGHT - 30, WIDTH//2 + 15, HEIGHT - 10,
+            fill="white")
         self.bullets = []
         self.enemies = []
 
         self.root.bind("<Left>", self.move_left)
         self.root.bind("<Right>", self.move_right)
         self.root.bind("<space>", self.shoot)
+
+    def move_left(self, event):
+        self.canvas.move(self.player, -PLAYER_SPEED, 0)
+
+    def move_right(self, event):
+        self.canvas.move(self.player, PLAYER_SPEED, 0)
+
+    def shoot(self, event):
+        x1, y1, x2, y2 = self.canvas.coords(self.player)
+        bullet = self.canvas.create_rectangle(
+            (x1 + x2)//2 - 2, y1 - 10, (x1 + x2)//2 + 2, y1, fill="red")
+        self.bullets.append(bullet)
+
+    def spawn_enemies(self):
+        for i in range(5):
+            x = random.randint(0, WIDTH - 30)
+            enemy = self.canvas.create_rectangle(
+                x, 10, x + 30, 30, fill="green")
+            self.enemies.append(enemy)
